db9b1fa0f41c65d7f6aaaa74aa4792a75b078f1d
michellemnguyen <michellemnguyen@ucsb.edu>
2020-04-24 15:13:56 -0700

    Work on embedding youtube videos + travel page

--- portfolio/src/Components/Travel.js
+++ portfolio/src/Components/Travel.js
@@ -4,7 +4,27 @@ class Travel extends Component {
     render() {
         return (
             <div>
-                TRAVEL
+                <div className='page-title'>Travel</div>
+
+                {/* TWO VIDEOS, SIDE BY SIDE */}
+                <div className='travel-entry'>
+                    <div className='video-container'>
+                        <iframe title="1 Second/Day" src="https://www.youtube.com/embed/5_dbVMXtqNs" frameborder="0" width="100%" height="250%"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div className='video-container'>
+                        <iframe title="summer 2018" src="https://www.youtube.com/embed/BCly0NsWc-g" frameborder="0" width="100%" height="250%"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                {/* SWEDEN PHOTO, GOLDEN RULE */}
+
+                {/* DENMARK PHOTO, GOLDEN RULE */}
+
+                {/* AMSTERDAM PHOTO, GOLDEN RULE */}
+
+                {/* UK PHOTOS, GOLDEN RULE */}
             </div>
         );
     }
