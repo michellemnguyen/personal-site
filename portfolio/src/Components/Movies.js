d99ca1fa51a667183420426a949238cb76f30f66
michellemnguyen <michellemnguyen@ucsb.edu>
2020-05-15 12:25:02 -0700

    Successfully called API from code

--- portfolio/src/Components/Movies.js
+++ portfolio/src/Components/Movies.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import axios from "axios"; 
-import Lightbox from 'react-image-lightbox';
 import '../milligram.css'
 
-const omdb_api_key = '1bfcf4bf'
-const api_url = 'https://www.ombdapi.com/?apikey='+omdb_api_key+'&i=' // needs IMDB ID
+const api_url = 'http://www.omdbapi.com/?apikey=1bfcf4bf&i=' // needs IMDB ID
 
 let movies_list = ['tt0381707', 'tt0878804', 'tt0109830', 'tt0245429', 'tt0095016', 'tt0268978', 'tt0319343', 'tt0119177']
 // White Chicks, The Blind Side, Forrest Gump, Spirited Away, Die Hard, A Beautiful Mind, Elf, Gattaca
+
+// let options = {mode: 'cors', headers: { 'Access-Control-Allow-Origin': true }}
 
 class Movies extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            moviePosters: []
+            moviesList: []
         }
     }
 
@@ -35,9 +35,35 @@ class Movies extends Component {
             - Would store in state
         - If I do the above, should I dynamically load the images onto the page as well,
             or should I prestore the poster urls & load everything at runtime?
+        - Can probably use a map function to run through the poster URLs and put all the images onto the page
+        - Can also use map function to run through each of the imdb ID's and get each movie's information
     */
 
+    componentDidMount() {
+        movies_list.map((posterID) => {
+            axios.get(api_url+posterID)
+            .then(res => {
+                let movieObject = res.data
+                let newMovie = {
+                    title: movieObject.Title,
+                    directer: movieObject.Director,
+                    rating: movieObject.imdbRating,
+                    poster: movieObject.Poster
+                }
+                this.setState(prevState => ({
+                    moviesList: [...prevState.moviesList, newMovie]
+                }))
+            }).catch(err => {
+                console.log(err);
+            });
+            return (
+                console.log('Got data from: ' + api_url+posterID)
+            )
+        })
+    }
+
     render() {
+        
         return (
             <div>
                 
@@ -45,24 +71,16 @@ class Movies extends Component {
                     <h1>Movie Database</h1>
                 </div>
 
-                <div class='lowerBody'>
+                <div className='lowerBody'>
+                    <div className='article'> 
 
+                    movie posters, eventually
 
-
+                    </div>
                 </div>
 
             </div>
         );
-    }
-}
-
-async function getPosterSrc(posterID) {
-    try {
-        const response = await axios.get(api_url+posterID);
-        console.log(response);
-        return response;
-    } catch (error) {
-        console.error(error);
     }
 }
 
--- portfolio/src/Components/TabList.js
+++ portfolio/src/Components/TabList.js
@@ -3,10 +3,11 @@ import Tab from './Tab';
 
 class TabList extends Component {
     render() {
-        return this.props.tabs.map((tab) => (
+        return this.props.tabs.map((tab, id) => (
             <Tab tab={tab} 
                  activeTab={this.props.activeTab}
-                 changeTab={this.props.changeTab}/>
+                 changeTab={this.props.changeTab}
+                 key={id}/>
         ));
     }
 }
